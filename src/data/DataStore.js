676461bc3056424f0f1670818eb89627d6c49779
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2019-10-30 22:56:05 +0330

    Consuming the RESTful Web Service

--- src/data/DataStore.js
+++ src/data/DataStore.js
@@ -1,6 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { ShopReducer } from "./ShopReducer";
 import { CartReducer } from "./CartReducer";
 import { CommonReducer } from "./CommonReducer";
-
-export const SportsStoreDataStore = createStore(CommonReducer(ShopReducer, CartReducer));
+import { asyncActions } from "./AsyncMiddleware";
+export const SportsStoreDataStore
+    = createStore(CommonReducer(ShopReducer, CartReducer),
+    applyMiddleware(asyncActions));
