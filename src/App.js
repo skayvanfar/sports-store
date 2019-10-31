9d3d11323bdc30f2dea1767323a34aaf70de33c4
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2019-10-31 13:43:14 +0330

    Preparing the Application for Deployment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {Component} from "react";
+import React, { Component, lazy, Suspense } from "react";
 import {SportsStoreDataStore} from "./data/DataStore";
 import {Provider} from "react-redux";
 import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
 import {ShopConnector} from "./shop/ShopConnector";
-import {Admin} from "./admin/Admin";
 import {AuthProviderImpl} from "./auth/AuthProviderImpl";
+
+const Admin = lazy(() => import("./admin/Admin"));
 
 export default class App extends Component {
     render() {
@@ -13,7 +14,12 @@ export default class App extends Component {
                 <Router>
                     <Switch>
                         <Route path="/shop" component={ShopConnector}/>
-                        <Route path="/admin" component={Admin}/>
+                        <Route path="/admin" render={
+                            routeProps =>
+                                <Suspense fallback={ <h3>Loading...</h3> }>
+                                    <Admin { ...routeProps } />
+                                </Suspense>
+                        } />
                         <Redirect to="/shop"/>
                     </Switch>
                 </Router>
