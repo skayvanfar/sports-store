60d4a145e00b88cb8dbeaa6413df5a88419760f6
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2019-10-31 11:33:45 +0330

    Adding the Checkout Process

--- src/data/RestDataSource.js
+++ src/data/RestDataSource.js
@@ -3,10 +3,15 @@ import { RestUrls } from "./Urls";
 
 export class RestDataSource {
 
-    GetData = async(dataType, params) =>
+    constructor(err_handler) {
+        this.error_handler = err_handler || (() => {});
+    }
+    GetData = (dataType, params) =>
         this.SendRequest("get", RestUrls[dataType], params);
 
-    SendRequest = (method, url, params) => Axios.request({
-        method, url, params
-    });
+    StoreData = (dataType, data) =>
+        this.SendRequest("post", RestUrls[dataType], {}, data);
+
+    SendRequest = (method, url, params, data) =>
+        Axios.request({ method, url, params, data });
 }
--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -2,13 +2,15 @@ import React, {Component} from "react";
 import {Switch, Route, Redirect}
     from "react-router-dom"
 import {connect} from "react-redux";
-import {loadData} from "../data/ActionCreators";
+import {loadData, placeOrder} from "../data/ActionCreators";
 import {DataTypes} from "../data/Types";
 import {Shop} from "./Shop";
 import {addToCart, updateCartQuantity, removeFromCart, clearCart}
     from "../data/CartActionCreators";
 import {CartDetails} from "./CartDetails";
 import {DataGetter} from "../data/DataGetter";
+import { Checkout } from "./Checkout";
+import { Thanks } from "./Thanks";
 
 const mapStateToProps = (dataStore) => ({
     ...dataStore
@@ -16,7 +18,8 @@ const mapStateToProps = (dataStore) => ({
 
 const mapDispatchToProps = {
     loadData,
-    addToCart, updateCartQuantity, removeFromCart, clearCart
+    addToCart, updateCartQuantity, removeFromCart, clearCart,
+    placeOrder
 }
 
 // const filterProducts = (products = [], category) =>
@@ -39,6 +42,10 @@ export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
                        }/>
                 <Route path="/shop/cart" render={(routeProps) =>
                     <CartDetails {...this.props} {...routeProps} />}/>
+                <Route path="/shop/checkout" render={ routeProps =>
+                    <Checkout { ...this.props } { ...routeProps } /> } />
+                <Route path="/shop/thanks" render={ routeProps =>
+                    <Thanks { ...this.props } { ...routeProps } /> } />
                 <Redirect to="/shop/products/all/1"/>
             </Switch>
         }
