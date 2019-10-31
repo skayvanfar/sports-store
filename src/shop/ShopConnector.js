d41a1d4c6e111951ff5596ee83ff860610c2f134
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2019-10-31 11:45:34 +0330

    Simplifying the Shop Connector Component

--- src/shop/ShopConnector.js
+++ src/shop/ShopConnector.js
@@ -2,57 +2,46 @@ import React, {Component} from "react";
 import {Switch, Route, Redirect}
     from "react-router-dom"
 import {connect} from "react-redux";
-import {loadData, placeOrder} from "../data/ActionCreators";
+import * as ShopActions from "../data/ActionCreators";
 import {DataTypes} from "../data/Types";
-import {Shop} from "./Shop";
-import {addToCart, updateCartQuantity, removeFromCart, clearCart}
-    from "../data/CartActionCreators";
-import {CartDetails} from "./CartDetails";
+import {Shop} from "../shop/Shop";
+import * as CartActions from "../data/CartActionCreators";
+import {CartDetails} from "../shop/CartDetails";
 import {DataGetter} from "../data/DataGetter";
-import { Checkout } from "./Checkout";
-import { Thanks } from "./Thanks";
+import {Checkout} from "../shop/Checkout";
+import {Thanks} from "../shop/Thanks";
 
-const mapStateToProps = (dataStore) => ({
-    ...dataStore
-})
-
-const mapDispatchToProps = {
-    loadData,
-    addToCart, updateCartQuantity, removeFromCart, clearCart,
-    placeOrder
-}
-
-// const filterProducts = (products = [], category) =>
-// (!category || category === "All")
-// ? products
-// : products.filter(p =>
-// p.category.toLowerCase() === category.toLowerCase());
-
-export const ShopConnector = connect(mapStateToProps, mapDispatchToProps)(
+const mapDispatchToProps = {...ShopActions, ...CartActions};
+export const ShopConnector = connect(ds => ds, mapDispatchToProps)(
     class extends Component {
+        selectComponent = (routeProps) => {
+            const wrap = (Component, Content) =>
+                <Component {...this.props} {...routeProps}>
+                    {Content && wrap(Content)}
+                </Component>
+            switch (routeProps.match.params.section) {
+                case "products":
+                    return wrap(DataGetter, Shop);
+                case "cart":
+                    return wrap(CartDetails);
+                case "checkout":
+                    return wrap(Checkout);
+                case "thanks":
+                    return wrap(Thanks);
+                default:
+                    return <Redirect to="/shop/products/all/1"/>
+            }
+        }
+
         render() {
             return <Switch>
                 <Redirect from="/shop/products/:category"
                           to="/shop/products/:category/1" exact={true}/>
-                <Route path={"/shop/products/:category/:page"}
-                       render={(routeProps) =>
-                           <DataGetter {...this.props} {...routeProps}>
-                               <Shop {...this.props} {...routeProps} />
-                           </DataGetter>
-                       }/>
-                <Route path="/shop/cart" render={(routeProps) =>
-                    <CartDetails {...this.props} {...routeProps} />}/>
-                <Route path="/shop/checkout" render={ routeProps =>
-                    <Checkout { ...this.props } { ...routeProps } /> } />
-                <Route path="/shop/thanks" render={ routeProps =>
-                    <Thanks { ...this.props } { ...routeProps } /> } />
-                <Redirect to="/shop/products/all/1"/>
+                <Route path={"/shop/:section?/:category?/:page?"}
+                       render={routeProps => this.selectComponent(routeProps)}/>
             </Switch>
         }
 
-        componentDidMount() {
-            this.props.loadData(DataTypes.CATEGORIES);
-            //this.props.loadData(DataTypes.PRODUCTS);
-        }
+        componentDidMount = () => this.props.loadData(DataTypes.CATEGORIES);
     }
 )
